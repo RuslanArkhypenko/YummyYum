51a3efe8f046308bb0febab95916141ba67a01d6
Ruslan Arhypenko <ruslanarhypenko@ruslans-macbook-air.local>
2019-02-18 19:54:05 +0100

    Initial Commit

---
+++ YummyYum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YummyYum
+//
+//  Created by Ruslan Arhypenko on 2/18/19.
+//  Copyright © 2019 Ruslan Arhypenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
